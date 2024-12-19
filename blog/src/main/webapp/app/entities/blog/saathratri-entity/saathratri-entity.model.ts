afb8940233531824f55c9ad3fd02acbc3f5357d5
Amar Premsaran Patel <amarppatel1@gmail.com>
2024-12-18 20:00:22 -0500

    Got datetime and date fields holding thier values for non-primary-key fields..

--- blog/src/main/webapp/app/entities/blog/saathratri-entity/saathratri-entity.model.ts
+++
@@ -1,8 +0,0 @@
-export interface ISaathratriEntity {
-  entityId: string;
-  entityName?: string | null;
-  entityDescription?: string | null;
-  entityCost?: number | null;
-}
-
-export type NewSaathratriEntity = Omit<ISaathratriEntity, 'entityId'> & { entityId: string };
