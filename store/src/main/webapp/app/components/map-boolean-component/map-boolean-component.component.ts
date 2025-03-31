15bd030ee518308832b27667747f6d58cf82b2ea
Amar Premsaran Patel <amarppatel1@gmail.com>
2025-03-31 16:12:03 -0400

    Got saved maps populating.

--- store/src/main/webapp/app/components/map-boolean-component/map-boolean-component.component.ts
+++ store/src/main/webapp/app/components/map-boolean-component/map-boolean-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
@@ -13,7 +13,7 @@ import { EditBooleanDialogComponent } from '../edit-boolean-dialog-component/edi
   templateUrl: './map-boolean-component.component.html',
   styleUrls: ['./map-boolean-component.component.css'],
 })
-export class MapBooleanComponent implements OnChanges {
+export class MapBooleanComponent implements OnInit, OnChanges {
   @Input() inputFields: Record<string, boolean> = {};
   @Output() dataChange = new EventEmitter<Record<string, boolean>>();
 
@@ -31,8 +31,13 @@ export class MapBooleanComponent implements OnChanges {
     });
   }
 
-  get fields(): FormArray {
+  get formArray(): FormArray {
     return this.form.get('fields') as FormArray;
+  }
+
+  ngOnInit(): void {
+    // Disable all value fields in read-only mode
+    this.disableAllValueFields();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -44,30 +49,49 @@ export class MapBooleanComponent implements OnChanges {
   }
 
   populateFormArray(): void {
-    this.fields.clear();
+    this.formArray.clear();
     Object.entries(this.mapDetails).forEach(([key, value]) => {
-      this.fields.push(this.fb.group({ key: [key], value: [value, Validators.required] }));
+      const group = this.fb.group({
+        key: [key],
+        value: [value, Validators.required],
+      });
+      group.get('value')?.disable(); // Disable value field on init
+      this.formArray.push(group);
+    });
+  }
+
+  disableAllValueFields(): void {
+    this.formArray.controls.forEach(controlGroup => {
+      controlGroup.get('value')?.disable();
     });
   }
 
   getFormControl(index: number, controlName: string): FormControl {
-    return this.fields.at(index).get(controlName) as FormControl;
+    return this.formArray.at(index).get(controlName) as FormControl;
   }
 
   openEditBooleanDialog(index: number): void {
-    const field = this.fields.at(index).value;
+    const keyControl = this.getFormControl(index, 'key');
+    const valueControl = this.getFormControl(index, 'value');
+
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (!keyControl || !valueControl) return;
+
+    const key = keyControl.value;
+    const currentValue = valueControl.value;
+
     const dialogRef = this.dialog.open(EditBooleanDialogComponent, {
       width: '500px',
       height: '300px',
       maxHeight: '90vh',
-      data: { key: field.key, value: field.value },
+      data: { key, value: currentValue },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result: boolean | undefined) => {
       if (typeof result === 'boolean') {
-        this.getFormControl(index, 'value').setValue(result);
-        this.mapDetails[field.key] = result;
-        this.emitData();
+        valueControl.setValue(result); // update the form control
+        this.mapDetails[key] = result; // update internal map
+        this.emitData(); // notify parent
       }
     });
   }
@@ -76,18 +100,23 @@ export class MapBooleanComponent implements OnChanges {
     if (this.form.valid) {
       const { newKey, newValue } = this.form.value;
       const trimmedKey = newKey.trim();
+      const group = this.fb.group({
+        key: [trimmedKey],
+        value: [newValue, Validators.required],
+      });
+      group.get('value')?.disable(); // Disable the newly added row
       this.mapDetails[trimmedKey] = newValue;
-      this.fields.push(this.fb.group({ key: [trimmedKey], value: [newValue, Validators.required] }));
+      this.formArray.push(group);
       this.form.reset({ newKey: '', newValue: null });
       this.emitData();
     }
   }
 
   deleteRow(index: number): void {
-    const field = this.fields.at(index).value;
+    const field = this.formArray.at(index).value;
     // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
     delete this.mapDetails[field.key];
-    this.fields.removeAt(index);
+    this.formArray.removeAt(index);
     this.emitData();
   }
 
